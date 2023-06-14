d6722f87b31f5fd9c57fd778cf931787540c934e
abdallah-altay <altaay1@student.op.ac.nz>
2023-06-15 08:15:04 +1200

    feat: add player controller script

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * moveSpeed;
+        float vertical = Input.GetAxis("Vertical") * moveSpeed;
+        //Vector3 UserInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = new Vector2(horizontal, vertical);
+    }
+}
